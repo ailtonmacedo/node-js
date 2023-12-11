76bc3d7fa5495773589934c38a1eaf3c84eece16
Ailton Macedo <ailtonwsb@gmail.com>
2023-12-11 12:04:26 -0300

    feat: conhecendo o node:http

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,30 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
-  return res.end("Hello Mundão!");
+  //desestururando a requisição
+  const { method, url } = req;
+
+  console.log(req.headers);
+  console.log(res.headers);
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .writeHead(200)
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({ id: 1, nome: "Ailton Macedo", email: "emial@email.com" });
+
+    return res
+      .setHeader("Content-type", "application/json")
+      .end("Criação de usuário");
+  }
+
+  return res.writeHead(404).end("Not Found");
 });
 
 server.listen(3333);
